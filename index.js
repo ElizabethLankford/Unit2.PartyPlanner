db587d620addcb1162c052e53f42365ac51ef5b8
ElizabethLankford <elizabethlankford1@gmail.com>
2023-12-04 15:31:27 -0600

    added event info of guests and rsvp

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 // URL for API
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events`;
+const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF`;
 
 // Declared my state variables
 const state = {
   events: [],
+  event: null,
+  guests: [],
+  rsvps: [],
 };
 
 const eventContainer = document.querySelector("#eventContainer");
 const form = document.querySelector("#addEvent");
+const guestEl = document.querySelector("#guestInfo");
+const eventEl = document.querySelector("#eventInfo");
+const guestList = document.querySelector("#guestList");
+
 form.addEventListener("submit", createEvent);
 
+window.addEventListener("hashchange", selectEvent);
 // initiating function
 async function init() {
   await fetchEventData();
   await render();
+  await getGuests();
+  await getRsvps();
+
+  selectEvent();
 }
 init();
 // async function to fetch the event data from the API
@@ -39,7 +52,7 @@ async function render() {
     let newDate = new Date(event.date).toLocaleString();
     const li = document.createElement("li");
     li.innerHTML = `
-        <p><b>Event Name:</b> ${event.name}</p>
+        <p><b>Event Name:</b><a href=#${event.id}> ${event.name}</a></p>
         <p><b>Date & Time:</b> ${newDate}</p>
         <p><b>Address: </b>${event.location}</p>
     `;
@@ -138,4 +151,70 @@ async function editEvent(id) {
   } catch (error) {
     console.log(error);
   }
+}
+//get guest function
+
+async function getGuests() {
+  try {
+    const response = await fetch(`${API}/guests`);
+    const json = await response.json();
+    state.guests = json.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// render list of guest
+function renderGuests() {
+  const rsvps = state.rsvps.filter((rsvp) => rsvp.eventId === state.event.id);
+  const guestIds = rsvps.map((rsvp) => rsvp.guestId);
+  const guests = state.guests.filter((guest) => guestIds.includes(guest.id));
+
+  if (!guests.length) {
+    guestList.innerHTML = `
+<li>No guests yet!</li>
+    `;
+  }
+
+  const guestListArr = guests.map((guest) => {
+    const guestInfo = document.createElement("li");
+    guestInfo.innerHTML = `
+      <span>${guest.name}</span>
+      <span>${guest.email}</span>
+      <span>${guest.phone}</span>
+    `;
+
+    return guestInfo;
+  });
+  guestEl.replaceChildren(...guestListArr);
+}
+
+// get list of rsvps
+async function getRsvps() {
+  try {
+    const response = await fetch(`${API}/rsvps`);
+    const json = await response.json();
+    state.rsvps = json.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// selecting an event function
+function selectEvent() {
+  const id = window.location.hash.slice(1);
+  state.event = state.events.find((event) => event.id === +id);
+
+  if (!state.event) {
+    eventEl.innerHTML = "<p>Select an event to see guest information.</p>";
+  }
+  const newDate = new Date(state.event.date);
+  eventEl.innerHTML = `
+  <h3>${state.event.name}</h3>
+  <p><b>Date & Time:</b> ${newDate.toLocaleString()}</p>
+  <p><b>Address: </b>${state.event.location}</p>
+  <p>${state.event.description}</p>
+  
+  `;
+  renderGuests();
 }
